183c2feadcee6b59285245cb83c9e03808be5d54
crazyli008 <327368137@qq.com>
2019-12-04 21:27:16 +0800

    首页，登录，注册，列表页未完成

--- src/js/ES5/login.js
+++ src/js/ES5/login.js
@@ -33,58 +33,65 @@ $(function () {
             $(".gong").fadeOut(500);
         };
     });
+    // 二维码动画
+    $('.mark').hover(function(){
+        $('.move-erweima').stop().animate({left:0},500,function(){
+            $('.sao-sao').css('display','block');
+        });
+    },function(){
+        $('.sao-sao').css('display','none');
+        $('.move-erweima').stop().animate({left:65},500);
+    })
+    // 密码登录更安全切换
+    $('.rt-top-ico').click(function(){
+        $(this).toggleClass('rt-top-ico-tog');
+        $('.rt-top-msg-tog').toggleClass('rt-top-msg-tog1');
+        $('.saoma').toggleClass('saoma1');
+        $('.login').toggleClass('login1');
+        
+    });
+    //普通登录和手机验证码登录
+    $('.switch-right').click(function(){
+        $('.switch-right').toggleClass('switch-right1');
+        $('.switch-left').toggleClass('switch-left1');
+        $('.simple-login').toggleClass('simple-login1');
+        $('.phone-login').toggleClass('phone-login1')
+    })
+    $('.switch-left').click(function(){
+        $('.switch-left').toggleClass('switch-left1');
+        $('.switch-right').toggleClass('switch-right1');
+        $('.simple-login').toggleClass('simple-login1');
+        $('.phone-login').toggleClass('phone-login1')
+    })
 
     // 验证手机号
-    $('.phone input').blur(function () {
+    $('.simple-login input').eq(0).blur(function () {
         var re = /^1\d{10}$/;
         var str = $(this).val();
         if (!re.test(str)) {
-            $('.phone .phone-msg').css('display', 'block');
+            $('.simple-login .p1').css('display', 'block');
         } else {
-            $('.phone .phone-msg').css('display', 'none');
+            $('.simple-login .p1').css('display', 'none');
         }
     })
-    // 验证密码强度
-    $('.pwd input').blur(function () {
+    // 验证密码格式
+    $('.simple-login input').eq(1).blur(function () {
         var re = /^[a-zA-Z\d]{6,20}$/;
         var str = $(this).val();
-        // 验证格式是否正确
-        if (re.test(str)) {
-            $('.pwd .pwmsg').css('display', 'none');
+        if (!re.test(str)) {
+            $('.simple-login .p2').css('display', 'block');
         } else {
-            $('.pwd .pwmsg').css('display', 'block');
+            $('.simple-login .p2').css('display', 'none');
         }
-        var mode = 0;
-        if ((/[0-9]+/g).test(str)) {
-            mode++;
-        }
-        if ((/[a-z]+/g).test(str)) {
-            mode++;
-        }
-        if ((/[A-Z]+/g).test(str)) {
-            mode++;
-        }
-        // 判断密码等级
-        if (mode > 0 && mode < 2) {
-            $('.pwd .strong span').eq(0).css('background', 'red');
-        }
-        if (mode > 1 && mode < 3) {
-            $('.pwd .strong span').eq(0).css('background', 'yellow');
-            $('.pwd .strong span').eq(1).css('background', 'yellow');
-        }
-        if (mode > 2) {
-            $('.pwd .strong span').eq(0).css('background', 'green');
-            $('.pwd .strong span').eq(1).css('background', 'green');
-            $('.pwd .strong span').eq(2).css('background', 'green');
-        }
-        mode = 0;
-    });
-    //点击注册
-    $('.submit input').click(function () {
+    })
+
+
+    // 点击登录
+    $('.simple-login input').eq(2).click(function () {
         // 获取手机号
-        var phone = $('.phone input').val();
+        var phone = $('.simple-login input').eq(0).val();
         // 获取密码
-        var pwd = $('.pwd input').val();
+        var pwd = $('.simple-login input').eq(1).val();
         // 验证手机号，和密码不能为空
         if (!phone || !pwd) {
             alert('您填写的信息有误！');
@@ -102,7 +109,7 @@ $(function () {
             alert('您的密码格式有误！请重新输入！');
             return;
         }
-        // 如果上述验证都没问题，开始将新用户的信息存入cookie中
+        // 如果上述验证都没问题，开始找cookie中有没有注册过的用户信息
         // 先获取cookie中的submit-msg这个key值，将它的value返回，没有，就返回空串
         var cookieValueStr = $.cookie('submit-msg') ? $.cookie('submit-msg') : '';
         // 将字符串转为json对象
@@ -114,15 +121,21 @@ $(function () {
         }
         // 将获取到的value信息，是字符串，将字符串转为转为对象
         var cookieValueObj = convertCookieToCookieObj(cookieValueStr);
-        // 判断对象中是否存在接收回来的数据中用户名
+        // 在value中找信息
         if (phone in cookieValueObj) {
-            alert('手机号已存在！不能重新出册');
+            if(cookieValueObj[phone] === pwd){//cookie中的密码，和用户输入的密码是否一致，如果一致，登录成功跳转页面
+                $.cookie('load',phone,{expires:7,path:'/'});//登录成功，就将这个登陆信息，保存3天
+                alert('登录成功！');
+                location.href = '../index.html';//登录成功后，跳转的产品页面
+                return;
+
+            }else{
+                alert('密码错误！')
+            }
+        }else{
+            alert('用户名不存在！请注册');
             return;
         }
-        cookieValueObj[phone] = pwd;
-        $.cookie('submit-msg',JSON.stringify(cookieValueObj),{expires:7,path:'/'});
-        // 完成后清空框里的值
-        phone = pwd ='';
-        alert('您已经注册成功，请登录！');
     })
+
 })
